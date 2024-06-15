c9dbc69f0de76d17259d7631a32a5175e3de3c2d
Thosea <104597976+ItsThosea@users.noreply.github.com>
2024-06-15 16:47:13 -0400

    1.2.0

--- src/main/java/me/thosea/eatserverpacks/EatServerPackPolicy.java
+++
@@ -1,7 +0,0 @@
-package me.thosea.eatserverpacks;
-
-import net.minecraft.client.network.ServerInfo.ResourcePackPolicy;
-
-public interface EatServerPackPolicy {
-	ResourcePackPolicy INSTANCE = ResourcePackPolicy.valueOf("esp$EAT_SERVER_PACK");
-}
