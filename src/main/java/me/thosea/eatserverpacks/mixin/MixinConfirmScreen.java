7572bea3bcea7469170d9be1a76462acc4b60ee2
Thosea <104597976+ItsThosea@users.noreply.github.com>
2024-06-15 16:47:13 -0400

    1.2.0

--- src/main/java/me/thosea/eatserverpacks/mixin/MixinConfirmScreen.java
+++ src/main/java/me/thosea/eatserverpacks/mixin/MixinConfirmScreen.java
@@ -1,20 +1,28 @@
 package me.thosea.eatserverpacks.mixin;
 
 import it.unimi.dsi.fastutil.booleans.BooleanConsumer;
-import me.thosea.eatserverpacks.EatServerPackPolicy;
+import me.thosea.eatserverpacks.EatServerPacks;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ConfirmScreen;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.tooltip.Tooltip;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.network.packet.c2s.common.ResourcePackStatusC2SPacket.Status;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableTextContent;
+import net.minecraft.util.Util;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.net.URL;
+import java.util.Map.Entry;
 
 @Mixin(ConfirmScreen.class)
 public abstract class MixinConfirmScreen extends Screen {
@@ -43,6 +51,9 @@ public abstract class MixinConfirmScreen extends Screen {
 		}
 	}
 
+	private ButtonWidget esp$eatPackButton;
+	private ButtonWidget esp$downloadPackButton;
+
 	@Inject(method = "addButtons", at = @At("TAIL"))
 	private void onAddButtons(int y, CallbackInfo ci) {
 		if(!esp$isResourcePack) return;
@@ -50,19 +61,61 @@ public abstract class MixinConfirmScreen extends Screen {
 		ClientPlayNetworkHandler network = MinecraftClient.getInstance().getNetworkHandler();
 		if(network != null
 				&& network.getServerInfo() != null
-				&& network.getServerInfo().getResourcePackPolicy() == EatServerPackPolicy.INSTANCE) {
+				&& network.getServerInfo().getResourcePackPolicy() == EatServerPacks.PACK_POLICY) {
 			this.close();
 			this.esp$eatServerPack();
 			return;
 		}
 
-		addButton(ButtonWidget.builder(
-						Text.literal("Eat Server Pack"),
+		addButton(esp$eatPackButton = ButtonWidget.builder(
+						Text.translatable("eatserverpack"),
 						button -> {
 							client.setScreen(null);
 							esp$eatServerPack();
 						})
-				.dimensions(this.width / 2 - 155 + 80, y + 25, 150, 20).build());
+				.tooltip(Tooltip.of(Text.translatable("eatserverpack.tooltip")))
+				.dimensions(this.width / 2 - 155 + 80, y + 25, 150, 20)
+				.build());
+
+		URL url = EatServerPacks.currentPackUrl;
+		EatServerPacks.currentPackUrl = null;
+		addButton(esp$downloadPackButton = ButtonWidget.builder(
+						Text.translatable("eatserverpack.download"),
+						button -> {
+							if(url != null) {
+								Util.getOperatingSystem().open(url);
+							}
+						})
+				.tooltip(Tooltip.of(Text.translatable("eatserverpack.download.tooltip")))
+				.dimensions(this.width / 2 - 155 + 80, y + 25, 150, 20)
+				.build());
+
+		// wait for tick
+		esp$eatPackButton.visible = false;
+		esp$downloadPackButton.visible = false;
+	}
+
+	@Inject(method = "tick", at = @At("HEAD"))
+	private void onTick(CallbackInfo ci) {
+		if(!esp$isResourcePack) return;
+
+		boolean isSneakPressed = false;
+
+		for(Entry<InputUtil.Key, KeyBinding> entry : KeyBinding.KEY_TO_BINDINGS.entrySet()) {
+			if(entry.getValue() == client.options.sneakKey) {
+				int state = GLFW.glfwGetKey(client.getWindow().getHandle(), entry.getKey().getCode());
+				isSneakPressed = state == GLFW.GLFW_PRESS;
+				break;
+			}
+		}
+
+		if(isSneakPressed) {
+			esp$eatPackButton.visible = false;
+			esp$downloadPackButton.visible = true;
+		} else {
+			esp$eatPackButton.visible = true;
+			esp$downloadPackButton.visible = false;
+		}
 	}
 
 	private void esp$eatServerPack() {
